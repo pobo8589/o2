4d659891d9f4160f061f762cf63881d6bbeaa5de
pobo <pobo8589@interia.pl>
2020-11-12 14:31:31 +0100

    first

---
+++ stylee.cs
@@ -0,0 +1,192 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+header{
+    position: relative;
+    width: 100%;
+    height: 10vh;
+    
+}
+header div{
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-top: 10px;
+}
+
+.o2{
+    display: inline-block;
+    width: 70px;
+    height: 70px;
+    margin-right: 58px;
+    float: left;
+}
+.logo{
+    width: 100%;
+    height: 100%;
+
+}
+.pogoda{
+    height: 43px;
+    margin-right:10px;
+    float: left;
+}
+.weather{
+    margin-right: 10px;
+    font-size: 8px;
+}
+.description{
+    float:right;
+    
+}
+.img{
+    width: 100%;
+    height: 100%;
+}
+.menu{
+    float: right;
+    display: inline-block;
+}
+.headermenu{
+    float: left;
+    margin-right: 20px;
+}
+section{
+    padding-top: 20px;
+    
+}
+section div{
+    margin: 0 auto;
+}
+.advertisement{
+    background-color: orange;
+    height: 25vh;
+    width: 60vw;
+    margin: 0 auto;
+    margin-bottom: 20px;
+}
+.advertisement p{
+    color: white;
+    font-size: 40px;
+}
+.announcements{
+    padding-top: 20px;
+    margin: 0 auto;
+    width: 80vw;
+  
+}
+.wrap1{
+
+  
+
+}
+.des .img{
+display: inline-block;
+float: left;
+
+
+}
+.des{
+    background-color: white;
+    width: 39%;
+    height: 48vh;
+    float: left;
+    margin-right: 10px;
+}
+.wrap1 .text a{
+    color: black;
+    text-decoration: none;
+    font-size: 28px;
+    font-weight: 300;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.image img{
+    width: 100%;
+    
+}
+.text{
+    margin-bottom: 30px;
+    margin-left: 20px;
+    margin-top: 10px;
+}
+.text a:hover{
+    text-decoration:underline;
+}
+.refy{
+    margin-top: 20px;
+    background-color: orange;
+    width: 20%;
+    height: 86vh;
+    float: right;
+}
+.desc{
+    margin-top: 10px;
+    background-color: white;
+    width: 26%;
+    height: 31vh;
+    float: left;
+    margin-right: 5px;
+}
+.text a{
+    color: black;
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: 300;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.ggg{
+    width: 26%;
+    height: 63vh;
+    background-color: orange;
+    float: left;
+    margin-right: 5px;
+    margin-top: 10px;
+   
+}
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: block;
+  }
+.aaaa{
+    margin-top: 10px;
+    padding-top: 20px;
+    background-color: orange;
+    height: 25vh;
+    width: 60vw;
+    margin: 0 auto;
+}
+.wrap2 p{
+    color: purple;
+    font-size: 30px;
+}
+.wrap2{
+    padding-top: 20px;
+    margin: 0 auto;
+    width: 80vw;
+    background-color: grey;
+    margin-bottom: 10px;
+    padding-bottom: 30px;
+  
+}
+.baner2{
+    padding-top: 20px;
+    margin: 0 auto;
+    height: 50vh;
+    width: 80vw;
+    background-color: orange;
+  
+}
+.baner2 p{
+    color: white;
+    font-size:80px;
+}
+.baner3{
+    margin-top: 10px;
+    padding-top: 20px;
+    background-color: orange;
+    height: 25vh;
+    width: 60vw;
+    margin: 0 auto;
+}
